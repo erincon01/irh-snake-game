daa0b33c20d4f8f908a35a8c44830fb7aa73ec1b
erincon01 <eladio.rincon@gmail.com>
2024-06-28 19:03:02 +0200

    chore: Update configuration file and main script

--- README.md
+++ README.md
@@ -6,11 +6,17 @@
 
 ## setup
 
-## dependencias
+## dependencies
 
 pip install arcade pyinstaller
 https://www.pygame.org/news
 
 https://pyglet.readthedocs.io/en/latest/programming_guide/quickstart.html
 pip install pyglet
+
+https://geekflare.com/python-game-development-libraries-frameworks/
+
+- esto es una prueabaç
+- adslkj adñlfjas
+- adsf sadfad
 
--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import configparser
 import pyglet
-from pyglet import shapes
+from pyglet import shapes, window
+import pyglet.clock as clock
 
 # Inicializar y leer el archivo de configuración
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 # Cargar valores de configuración
-width = config['SCREEN']['width']
-height = config['SCREEN']['height']
-title = config['SCREEN']['title']
+width = config['Configuracion Pantalla']['width']
+heigh = config['Configuracion Pantalla']['height']
+title = config['Configuracion Pantalla']['title']
+movement = config['Configuracion Pantalla']['movement']
+circle_size = config['Configuracion Pantalla']['circle_size']
+# Convertir valores a enteros
+width = int(width)
+heigh = int(heigh)
+movement = int(movement)
+circle_size = int(circle_size)
+direction = "right"
 
 # Crear la ventana de Pyglet
-window = pyglet.window.Window(width=int(width), height=int(height), 
+window = window.Window(width=width, height=heigh, 
                               caption=title, resizable=False)
 
 # Crear la etiqueta de texto
@@ -26,11 +35,50 @@ label = pyglet.text.Label('circulo',
 batch = pyglet.graphics.Batch()
 
 # Crear un círculo rojo
-circle_size = 100
+circle_size = circle_size
 circle = shapes.Circle(x=window.width // 2,
                        y=window.height // 2,
                        radius=circle_size // 2,
                        color=(255, 0, 0), batch=batch)
+
+# Crear un rectángulo amarillo
+rectangle = shapes.Rectangle(x=0, y=0, width=window.width, height=50, color=(255, 255, 0), batch=batch)
+
+# crea un rombo verde
+diamond = shapes.Rectangle(x=0, y=0, width=window.width, height=50, color=(0, 255, 0), batch=batch)
+
+def callback(dt):
+    global direction
+    if direction == "right":
+        circle.x += movement
+        label.x += movement
+        # if circle.x > window.width:
+        #     circle.x = 0
+        #     label.x = 0
+        #     direction = "down"
+    elif direction == "down":
+        circle.y -= movement
+        label.y -= movement
+        # if circle.y < 0:
+        #     circle.y = window.height
+        #     label.y = window.height
+        #     direction = "left"
+    elif direction == "left":
+        circle.x -= movement
+        label.x -= movement
+        # if circle.x < 0:
+        #     circle.x = window.width
+        #     label.x = window.width
+        #     direction = "up"
+    elif direction == "up":
+        circle.y += movement
+        label.y += movement
+        # if circle.y > window.height:
+        #     circle.y = 0
+        #     label.y = 0
+        #     direction = "right"
+
+clock.schedule_interval(callback, 0.5)
 
 # Definir el evento on_draw
 @window.event
@@ -39,22 +87,40 @@ def on_draw():
     pyglet.gl.glClearColor(0.5, 0.5, 1, 1)
     batch.draw()
     label.draw()
+    rectangle.draw()
+    diamond.draw()
 
-# Definir el evento on_key_press para manejar las teclas presionadas
+# función que indica que hacer cuando se presiona una tecla
 @window.event
 def on_key_press(symbol, modifiers):
+    global direction
+    global movement
     if symbol == pyglet.window.key.RIGHT:
-        circle.x += 25
-        label.x += 25
+        if direction == "right":
+            movement += 10
+        direction = "right"
     elif symbol == pyglet.window.key.LEFT:
-        circle.x -= 25
-        label.x -= 25
+        if direction == "left":
+            movement += 10
+        direction = "left"
     elif symbol == pyglet.window.key.UP:
-        circle.y += 25
-        label.y += 25
+        if direction == "up":
+            movement += 10
+        direction = "up"
     elif symbol == pyglet.window.key.DOWN:
-        circle.y -= 25
-        label.y -= 25
+        if direction == "down":
+            movement += 10
+        direction = "down"
+
+# @window.event
+# def on_mouse_motion(x, y, dx, dy):
+#     # Actualizar la posición del círculo a las coordenadas del mouse
+#     circle.x = x
+#     circle.y = y
+#     # Actualizar la posición del rectángulo a las coordenadas del mouse
+#     rectangle.x = x
+#     rectangle.y = y
 
 # Ejecutar la aplicación
 pyglet.app.run()
+
